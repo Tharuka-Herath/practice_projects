082bd671260b8eaf44a6f2a7ea56e1d8a6abe23d
Tharuka <tharukaherath3201@gmail.com>
2023-06-12 12:30:40 +0530

    scrolling effect

--- Scrolling animation/script.js
+++ Scrolling animation/script.js
@@ -5,8 +5,10 @@ window.addEventListener('scroll', checkBoxes)
 
 function checkBoxes(){
     const triggerBottom= window.innerHeight/5*4;
+   
     boxes.forEach(box=>{
         const boxTop = box.getBoundingClientRect().top
+        
 
         if(boxTop<triggerBottom){
             box.classList.add("show")
